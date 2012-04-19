b9a8dc9b45c106465ebffb1bf32e32383524e266
natsualviss <sophie.grebaut@gmail.com>
2012-04-19 11:11:54 +0200

    Initial commit

---
+++ MoodleXmlApi/src/main/java/universite/toulouse/moodlexmlapi/tartopom/impl/App.java
@@ -0,0 +1,13 @@
+package universite.toulouse.moodlexmlapi.tartopom.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
