6a2207746d975726b17537f1fa0348a291307411
rvermunt <rvermunt54@gmail.com>
2012-05-27 00:11:03 +0200

    working on issue #11

--- MoodleXmlApi/src/main/java/universite/toulouse/moodlexmlapi/tartopom/impl/model/QuestionCalculated.java
+++ MoodleXmlApi/src/main/java/universite/toulouse/moodlexmlapi/tartopom/impl/model/QuestionCalculated.java
@@ -1,5 +1,40 @@
 package universite.toulouse.moodlexmlapi.tartopom.impl.model;
 
+import javax.xml.bind.annotation.XmlElement;
+
+import org.eclipse.persistence.oxm.annotations.XmlDiscriminatorValue;
+
+import universite.toulouse.moodlexmlapi.core.data.QuestionType;
+
+@XmlDiscriminatorValue("Calulated")
 public class QuestionCalculated extends QuestionBase {
+
+
+    private boolean shuffle;
+
+    private DatasetDefinitions datasetDefinitions;
+
+    public QuestionCalculated(){
+        super();
+        super.setQuestionType(QuestionType.calculated);
+    }
+
+    @XmlElement(name="shuffleanswers")
+    public boolean isShuffle() {
+        return shuffle;
+    }
+
+    public void setShuffle(boolean shuffle) {
+        this.shuffle = shuffle;
+    }
+
+    @XmlElement(name="dataset_definitions")
+    public DatasetDefinitions getDatasetDefinitions() {
+        return datasetDefinitions;
+    }
+
+    public void setDatasetDefinitions(DatasetDefinitions datasetDefinitions) {
+        this.datasetDefinitions = datasetDefinitions;
+    }
 
 }
