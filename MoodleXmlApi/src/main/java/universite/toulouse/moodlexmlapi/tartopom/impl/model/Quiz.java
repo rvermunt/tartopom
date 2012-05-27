1fd7a4d15904f1b922078cbd0419649cce9e4f32
Natsu Alviss <sophie.grebaut@gmail.com>
2012-05-27 21:34:40 +0200

    modification mapping quiz : jaxb ne supporte pas les interfaces pour les elements

--- MoodleXmlApi/src/main/java/universite/toulouse/moodlexmlapi/tartopom/impl/model/Quiz.java
+++ MoodleXmlApi/src/main/java/universite/toulouse/moodlexmlapi/tartopom/impl/model/Quiz.java
@@ -1,26 +1,45 @@
 package universite.toulouse.moodlexmlapi.tartopom.impl.model;
 
-
+import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 import universite.toulouse.moodlexmlapi.core.data.Question;
 
 /**
  * Classe Quiz, composée d'une liste de question
  * @author rvermunt
  */
-@XmlRootElement(name="quiz")
-public class Quiz implements universite.toulouse.moodlexmlapi.core.data.Quiz{
+@XmlRootElement(name = "quiz")
+public class Quiz implements universite.toulouse.moodlexmlapi.core.data.Quiz {
 
-    private List<Question> questionList;
+    @XmlElement(name = "question")
+    private List<QuestionBase> questionList;
 
     /**
      * @return questionList
      */
-    @XmlElement(name="question")
-    public List<Question> getQuestionList(){
-        return this.questionList;
+    @Override
+    @XmlTransient
+    public List<Question> getQuestionList() {
+
+        List<Question> questions = new ArrayList<Question>();
+        for (Question q : questionList) {
+            questions.add(q);
+        }
+
+        return questions;
+    }
+
+    public void setQuestionList(List<Question> questions) {
+
+        questionList = new ArrayList<QuestionBase>();
+        for (Question q : questions) {
+            questionList.add((QuestionBase) q);
+        }
     }
 
 }
